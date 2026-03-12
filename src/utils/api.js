b6552319be061414b77a201f35fc51c123712b97
Divyasree954 <23691A0542@mits.ac.in>
2026-03-12 16:06:01 +0530

    fix build

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://quiz-backend-b63l.onrender.com/api',
+  baseURL: 'https://quiz-backend-b63l.onrender.com',
   headers: { 'Content-Type': 'application/json' }
 });
 
