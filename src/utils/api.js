069ef2896425b5133d936bdbeb214081c54867f4
Divyasree954 <23691A0542@mits.ac.in>
2026-03-12 16:09:40 +0530

    add api in be url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://quiz-backend-b63l.onrender.com',
+  baseURL: 'https://quiz-backend-b63l.onrender.com/api',
   headers: { 'Content-Type': 'application/json' }
 });
 
